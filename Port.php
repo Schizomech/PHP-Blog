0885608d2a98f8003ab825689b0283b9ce457042
romeo <romeo.arisona@gmail.com>
2025-11-28 15:07:37 +0100

    uhh no idea what i changed

--- Port.php
+++ Port.php
@@ -17,7 +17,7 @@ require_once "manager.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Port</title>
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
